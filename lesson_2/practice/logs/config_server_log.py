1524a340f0ebf3e76e1a0c67d3e89bce832ffc33
Joksar <politer72@gmail.com>
2022-02-16 17:34:16 +0300

    ДЗ урок 2

---
+++ lesson_2/practice/logs/config_server_log.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import logging
+import logging.handlers
+sys.path.append('../')
+from common.variables import LOGGING_LEVEL
+
+# создание формировщика логов
+SERVER_FORMATTER = logging.Formatter('%(asctime)s %(levelname)-8s %(filename)s %(message)s')
+
+# Подготовка имени файла для логирования
+PATH = os.path.dirname(os.path.abspath(__file__))
+PATH = os.path.join(PATH, 'server.log')
+
+# создание потоков вывода логов
+STREAM_HANDLER = logging.StreamHandler(sys.stderr)
+STREAM_HANDLER.setFormatter(SERVER_FORMATTER)
+STREAM_HANDLER.setLevel(logging.ERROR)
+LOG_FILE = logging.handlers.TimedRotatingFileHandler(PATH, encoding='utf-8', interval=24, when='H')
+LOG_FILE.setFormatter(SERVER_FORMATTER)
+
+# создание и настройка регистратора
+LOGGER = logging.getLogger('server')
+LOGGER.addHandler(STREAM_HANDLER)
+LOGGER.addHandler(LOG_FILE)
+LOGGER.setLevel(LOGGING_LEVEL)
+
+# отладка
+if __name__ == '__main__':
+    LOGGER.critical('Критическая ошибка')
+    LOGGER.error('Ошибка')
+    LOGGER.debug('Отладочная информация')
+    LOGGER.info('Информационное сообщение')
